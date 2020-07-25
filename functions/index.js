593877ff22a309b6cb5958a0c05367ce9afe55a9
Chris Creighton (@creighcl) <27293513+Creighcl@users.noreply.github.com>
2020-07-25 18:47:44 -0400

    cra

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,20 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const uuid = require('uuid').v1;
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+const handleHelloWorld = (request, response) => {
+    functions.logger.info("Hello logs!", {structuredData: true});
+    response.send("Hello from Firebase!");
+};
+
+const handleCreateKey = (request, response) => {
+    const newKey = uuid();
+    const msgRoot = admin.database().ref('/boards/' + newKey);
+    msgRoot.set({ created: Date.now() });
+    response.status(200).send(newKey);
+};
+
+exports.helloWorld = functions.https.onRequest(handleHelloWorld);
+exports.createKey = functions.https.onRequest(handleCreateKey);
---
+++ functions/test.js
@@ -0,0 +1,6 @@
+
+const uuid = require('uuid').v1;
+
+const newKey = uuid();
+
+console.log(newKey);
